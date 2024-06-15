8c2aae0312cc78fec0d4915cd4ebbe9c92313ae5
4mser <nicolas.moreno.sagan@gmail.com>
2024-06-14 21:38:52 -0400

    sada

--- components/Initial.tsx
+++ components/Initial.tsx
@@ -23,7 +23,7 @@ const pages = [
     title: "Charts",
     component: (
       <div className="w-full h-[100dvh] overflow-y-auto flex flex-col md:px-56 md:pt-20 ">
-        <VisualizationPage />
+        {/* <VisualizationPage /> */}
         {/* <CombinedVisualizationPage /> */}
       </div>
     ),
