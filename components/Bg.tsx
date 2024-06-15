e37fe59b87a9501384e7e23302dc9c94e2892e12
4mser <nicolas.moreno.sagan@gmail.com>
2024-06-14 21:10:43 -0400

    asd

--- components/Bg.tsx
+++ components/Bg.tsx
@@ -1,7 +1,7 @@
-
+'use client'
 const Bg = () => {
   return (
-    <div className='w-full h-[100vh] scale-150 fixed left-0 -z-10'>
+    <div className='w-full h-[100dvh] scale-150 fixed left-0 -z-10'>
       <div className='w-full h-full  absolute light:bg-white dark:bg-black transition-colors duration-300 ease-in-out'></div>
     </div>
   );
